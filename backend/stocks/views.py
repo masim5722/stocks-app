cdf6eaacd5780a3982b29dc0d49e42a3b846d3f7
masim5722 <masim5722@gmail.com>
2021-10-24 16:26:26 +0500

    Commit # 4 Backend App - created api for getting all stocks sorted by CLOSE

--- backend/stocks/views.py
+++ backend/stocks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from .models import Stock
+from .serializers import StockSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def stock_list(request):
+    if request.method == 'GET':
+        stocks = Stock.objects.order_by('close').all()
+        serializer = StockSerializer(stocks, many=True)
+        return JsonResponse(serializer.data, safe=False)
